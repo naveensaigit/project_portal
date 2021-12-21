b9667c678f541b919025de5315b5060c1f707dca
lazyXghost <rjtbnslrealme@gmail.com>
2021-12-21 22:03:40 +0530

    Project Database setup

--- home/views.py
+++ home/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from .models import Project
 @login_required
 def main(request):
     context = {
-        'title': 'Home'
+        'title': 'Home',
+        'projects': Project.objects.all()
     }
     return render(request, 'home/main.html', context)
