e8f2d957acd9c092f5ee92754364f8599235ff55
lazyXghost <rjtbnslrealme@gmail.com>
2021-12-23 09:28:30 +0530

    Removed useless lines and added small changes

--- home/models.py
+++ home/models.py
@@ -3,14 +3,14 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 STATUS_CHOICES = (
-    ('1','Open'),
-    ('2','Ongoing'),
-    ('3','Completed')
+    ('Open','Open'),
+    ('Ongoing','Ongoing'),
+    ('Completed','Completed')
 )
 DIFFICULTY_CHOICES = (
-    ('1','Beginner'),
-    ('2','Intermediate'),
-    ('3','Hard')
+    ('Beginner','Beginner'),
+    ('Intermediate','Intermediate'),
+    ('Hard','Hard')
 )
 
 # Create your models here.
@@ -19,8 +19,8 @@ class Project(models.Model):
     Description = models.TextField()
     FloatedBy = models.ForeignKey(User, on_delete=models.CASCADE)
     Mentors = models.ManyToManyField(User, related_name='Mentors')
-    Status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='Open')
-    Difficulty = models.CharField(max_length=1, choices=DIFFICULTY_CHOICES, default='Beginner')
+    Status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Open')
+    Difficulty = models.CharField(max_length=15, choices=DIFFICULTY_CHOICES, default='Beginner')
     PreRequisite = models.TextField()
     Duration = models.CharField(max_length=30)
     DatePosted = models.DateTimeField(default = timezone.now)
