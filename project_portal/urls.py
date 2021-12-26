8f2f7cad9161b5bb2f9454a70720df288cf8e4d2
lazyXghost <rjtbnslrealme@gmail.com>
2021-12-26 18:26:06 +0530

    oauth view setup

--- project_portal/urls.py
+++ project_portal/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('profile/', include('users.urls')),
     path('signup/', users_views.signup, name='signup'),
+    path('oauth/', users_views.oauth, name='oauth'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('home.urls')),
