0c16d7875426a3675658feb6197319b42e81d78b
Lavish Sachdeva <b20113@students.iitmandi.ac.in>
2021-12-27 01:37:43 +0530

    Some essential changes in database

--- home/forms.py
+++ home/forms.py
@@ -4,13 +4,42 @@ from .models import Project
 
 
 class ProjectRegisterForm(forms.ModelForm):
+    CHOICES = (
+        ('0','None'),
+        ('1', '1st Year CSE'),
+        ('2', '1st Year DSE'),
+        ('3', '1st Year EE'),
+        ('4', '1st Year ME'),
+        ('5', '1st Year CE'),
+        ('6', '1st Year BioE'),
+        ('7', '2nd Year CSE'),
+        ('8', '2nd Year DSE'),
+        ('9', '2nd Year EE'),
+        ('10', '2nd Year ME'),
+        ('11', '2nd Year CE'),
+        ('12', '2nd Year BioE'),
+        ('13', '3rd Year CSE'),
+        ('14', '3rd Year DSE'),
+        ('15', '3rd Year EE'),
+        ('16', '3rd Year ME'),
+        ('17', '3rd Year CE'),
+        ('18', '3rd Year BioE'),
+        ('19', '4th Year CSE'),
+        ('20', '4th Year DSE'),
+        ('21', '4th Year EE'),
+        ('22', '4th Year ME'),
+        ('23', '4th Year CE'),
+        ('24', '4th Year BioE'),
+    )
+    OpenedFor = forms.MultipleChoiceField(
+        label="Opened For", choices=CHOICES, required=True)
     class Meta:
         model = Project
-        fields = ['Title','Description','Mentors','Status','Difficulty','PreRequisite','Duration','SelectionCriteria','OpenedFor']
+        fields = ['Title','Description','Mentors','Status','OpenedFor','Difficulty','PreRequisite','Duration','SelectionCriteria']
 
 
 
 class ProjectUpdateForm(forms.ModelForm):
     class Meta:
         model = Project
-        fields = ['Title','Description','Mentors','Status','Difficulty','PreRequisite','Duration','SelectionCriteria','OpenedFor']
+        fields = ['Title','Description','Mentors','Status','OpenedFor','Difficulty','PreRequisite','Duration','SelectionCriteria']
--- users/forms.py
+++ users/forms.py
@@ -11,6 +11,30 @@ class UserRegisterForm(UserCreationForm):
 
 
 class ProfileRegisterForm(forms.ModelForm):
+    YEAR_CHOICES = (
+        ('1', '1st'),
+        ('2', '2nd'),
+        ('3', '3rd'),
+        ('4', '4th'),
+        ('5', '5th'),
+    )
+    BRANCH_CHOICES = (
+        ('1', 'CSE'),
+        ('2', 'DSE'),
+        ('3', 'ME'),
+        ('4', 'EE'),
+        ('5', 'CE'),
+        ('6', 'BioE'),
+    )
+    year = forms.ChoiceField(
+        label="Year", choices=YEAR_CHOICES, required=True,
+        widget=forms.Select(attrs={'yr': 'form-control input-sm'})
+    )
+    branch = forms.ChoiceField(
+        label="Branch", choices=BRANCH_CHOICES, required=True,
+        widget=forms.Select(attrs={'yr': 'form-control input-sm'})
+    )
+    
     class Meta:
         model = Profile
         fields = ['image', 'rollno', 'year', 'branch', 'techskills', 'cv']
@@ -23,6 +47,29 @@ class UserUpdateForm(forms.ModelForm):
 
 
 class ProfileUpdateForm(forms.ModelForm):
+    YEAR_CHOICES = (
+        ('1', '1st'),
+        ('2', '2nd'),
+        ('3', '3rd'),
+        ('4', '4th'),
+        ('5', '5th'),
+    )
+    BRANCH_CHOICES = (
+        ('1', 'CSE'),
+        ('2', 'DSE'),
+        ('3', 'ME'),
+        ('4', 'EE'),
+        ('5', 'CE'),
+        ('6', 'BioE'),
+    )
+    year = forms.ChoiceField(
+        label="Year", choices=YEAR_CHOICES, required=True,
+        widget=forms.Select(attrs={'yr': 'form-control input-sm'})
+    )
+    branch = forms.ChoiceField(
+        label="Branch", choices=BRANCH_CHOICES, required=True,
+        widget=forms.Select(attrs={'yr': 'form-control input-sm'})
+    )
     class Meta:
         model = Profile
         fields = ['image', 'rollno', 'year', 'branch', 'techskills', 'cv']
