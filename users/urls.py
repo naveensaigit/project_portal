b06ffb6be1657a1427e4e76bdbaec07d30e4dfe4
lazyXghost <rjtbnslrealme@gmail.com>
2021-12-29 13:15:47 +0530

    Added option to view other people's profile

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.profile, name = 'profile'),
+    path('<int:user_id>', views.profile, name = 'profile'),
     path('edit', views.profile_edit, name = 'profile-edit'),
     path('projects/applied', views.projects_applied, name = 'projects-applied'),
     path('projects/requested', views.projects_requested, name = 'projects-requested'),
