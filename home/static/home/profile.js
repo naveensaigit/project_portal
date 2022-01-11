442c73307783a636b5b8c9f048604f9307a7e90c
AvniMittal13 <avni.mittal2002@gmail.com>
2022-01-11 13:41:31 +0530

    profile card added

---
+++ home/static/home/profile.js
@@ -0,0 +1,23 @@
+alert("hello hello")
+
+// $(function () {
+
+//     $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+//         $('a[data-toggle="tab"]').removeClass('btn-primary');
+//         $('a[data-toggle="tab"]').addClass('btn-default');
+//         $(this).removeClass('btn-default');
+//         $(this).addClass('btn-primary');
+//     });
+
+//     $('.next').click(function () {
+//         var nextId = $(this).parents('.tab-pane').next().attr("id");
+//         $('[href=#' + nextId + ']').tab('show');
+//     });
+
+//     $('.prev').click(function () {
+//         var prevId = $(this).parents('.tab-pane').prev().attr("id");
+//         $('[href=#' + prevId + ']').tab('show');
+//     });
+
+// });
+
