16297300a5eab62653eb06e8d5e7af4db8e1cf43
lazyXghost <rjtbnslrealme@gmail.com>
2022-01-13 18:52:01 +0530

    Added functions

---
+++ functions.py
@@ -0,0 +1,144 @@
+from home.models import Project
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from users.models import Notification
+from django.core.mail import send_mail
+from django.conf import settings
+from django.shortcuts import redirect
+from django.contrib import messages
+
+def get_projects_id(request):
+    user_projects_id = []
+    user_starred_projects_id = []
+    user_requested_projects_id = []
+    user_liked_projects_id = []
+
+    all_project_list = Project.objects.all().order_by('-DatePosted')
+    user_applied_projects = all_project_list.filter(AlreadyApplied =  request.user)
+    user_floated_projects = all_project_list.filter(FloatedBy =  request.user)
+    user_requested_projects = all_project_list.filter(ApplyRequest = request.user)
+    user_starred_projects = request.user.profile.starred_projects.all()
+    user_liked_projects = request.user.profile.liked_projects.all()
+
+    for project in user_applied_projects:
+        user_projects_id.append(project.id)
+    for project in user_floated_projects:
+        user_projects_id.append(project.id)
+    for project in user_requested_projects:
+        user_requested_projects_id.append(project.id)
+    for project in user_starred_projects:
+        user_starred_projects_id.append(project.id)
+    for project in user_liked_projects:
+        user_liked_projects_id.append(project.id)
+    
+    return [user_projects_id, user_starred_projects_id, user_requested_projects_id, user_liked_projects_id]
+
+def get_paginated_projects(request, projects):
+    page = request.GET.get('page', 1)
+    paginator = Paginator(projects, 6)
+    try:
+        paginated_projects = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_projects = paginator.page(1)
+    except EmptyPage:
+        paginated_projects = paginator.page(paginator.num_pages)
+
+    return paginated_projects
+
+def apply_delimeter_seperation(projects):
+    for project in projects:
+        Flag=False
+        prereqs = str(project.PreRequisite).split('\r\n')
+        if(len(prereqs)>2):
+            Flag=True
+        prereqs= prereqs[0:2]
+        if(Flag==True):
+            prereqs.append("More Tags...")
+        project.PreRequisite = prereqs[0:3]
+
+def search(request, filtered_projects):
+    if request.method != "POST":
+        return filtered_projects
+    else:
+        value = request.POST['search']
+        if value == "":
+            return filtered_projects
+        else:
+            searched_projects_Title = Project.objects.filter(Title__contains = value)
+            searched_projects_Desription = Project.objects.filter(Description__contains = value)
+            searched_projects = searched_projects_Title | searched_projects_Desription
+            searched_projects = searched_projects.order_by('-DatePosted')
+            return searched_projects
+
+def send_notification(request, project):
+    project_id = request.GET.get('project_id')
+    current_user = request.user
+
+    notification_message=f"{current_user} has requested for {project} Project"
+    project_url = f"/project/?project_id={project_id}"
+    notification=Notification(user=project.FloatedBy, project_requested = project, notification_from = request.user, title= "Apply Request", message=notification_message,url=project_url)
+    notification.save()
+
+    if project.MailNotification == "On":
+        subject = 'Project Application'
+        message = notification_message
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [project.FloatedBy.email,]
+        send_mail( subject, message, email_from, recipient_list )
+
+def delete_notification(request, project):
+    Notification.objects.filter(notification_from = request.user).filter(project_requested = project).delete()
+
+def apply_on_project(request, project):
+    current_user = request.user
+    year= current_user.profile.year
+    branch=current_user.profile.branch
+    if(len(year)==0 or len(branch)==0):
+        messages.warning(request,"Please update your year and branch in profile section")
+        return redirect('/profile/edit')
+    else:
+        user_branch = f"{year} Year {branch}"
+        if(user_branch in project.OpenedFor):
+            project.ApplyRequest.add(current_user)
+            messages.success(request,"Successfully Requested!")
+            send_notification(request, project)
+        else:
+            messages.warning(request,"You are not eligible to opt this project.")
+
+def withdraw_from_project(request, project):
+    current_user = request.user
+    delete_notification(request, project)
+    project.ApplyRequest.remove(current_user)
+
+def leave_project(request, project):
+    current_user = request.user
+    if project.FloatedBy != current_user:
+        project.AlreadyApplied.remove(current_user)
+        messages.success(request,f"{project} is dropped successfully.")
+    else:
+        messages.error(request, 'You cannot leave this project because it is floated by you.')
+
+def do_simple_task(request, task):
+    project_id = request.GET.get('project_id')
+    task = request.GET.get('task')
+
+    project = Project.objects.get(id=project_id)
+    current_user = request.user
+
+    if task == "Apply":
+        apply_on_project(request, project)
+    elif task == "Withdraw":
+        withdraw_from_project(request,project)
+    elif task == "Leave":
+        leave_project(request, project)
+    elif task == "Star":
+        current_user.profile.starred_projects.add(project)
+    elif task == "Unstar":
+        current_user.profile.starred_projects.remove(project)
+    elif task == "Like":
+        current_user.profile.liked_projects.add(project)
+        project.Likes += 1
+        project.save()
+    elif task == "Unlike":
+        current_user.profile.liked_projects.remove(project)
+        project.Likes -= 1
+        project.save()
--- home/views.py
+++ home/views.py
@@ -1,16 +1,12 @@
-from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.core.mail import send_mail
-from django.conf import settings
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from home.decorators import user_is_project_author
-from users.models import Notification
 from .models import Project
 from .forms import ProjectRegisterForm, ProjectUpdateForm
 from .filters import ProjectFilter
+from functions import *
 
 @login_required
 def main(request):
@@ -18,71 +14,24 @@ def main(request):
     myFilter = ProjectFilter(request.GET,queryset=all_project_list)
     filtered_projects= myFilter.qs
 
-    if request.method == "POST":
-        value = request.POST['search']
-        if value == "":
-            searched_projects = filtered_projects
-        else:
-            searched_projects_Title = Project.objects.filter(Title__contains = value)
-            searched_projects_Desription = Project.objects.filter(Description__contains = value)
-            searched_projects = searched_projects_Title | searched_projects_Desription
-            searched_projects = searched_projects.order_by('-DatePosted')
-        projects = searched_projects
-    else:
-        projects = filtered_projects
+    projects = search(request, filtered_projects)
+    projects = get_paginated_projects(request, projects)
+    apply_delimeter_seperation(projects)
 
-    user_projects_id = []
-    user_starred_projects_id = []
-    user_requested_projects_id = []
-    user_liked_projects_id = []
-
+    projects_id = get_projects_id(request)
     user_applied_projects = all_project_list.filter(AlreadyApplied =  request.user)
     user_floated_projects = all_project_list.filter(FloatedBy =  request.user)
-    user_requested_projects = all_project_list.filter(ApplyRequest = request.user)
-    user_starred_projects = request.user.profile.starred_projects.all()
-    user_liked_projects = request.user.profile.liked_projects.all()
-
-    for project in user_applied_projects:
-        user_projects_id.append(project.id)
-    for project in user_floated_projects:
-        user_projects_id.append(project.id)
-    for project in user_requested_projects:
-        user_requested_projects_id.append(project.id)
-    for project in user_starred_projects:
-        user_starred_projects_id.append(project.id)
-    for project in user_liked_projects:
-        user_liked_projects_id.append(project.id)
-
-    page = request.GET.get('page', 1)
-    paginator = Paginator(projects, 6)
-    try:
-        projects = paginator.page(page)
-    except PageNotAnInteger:
-        projects = paginator.page(1)
-    except EmptyPage:
-        projects = paginator.page(paginator.num_pages)
-
-    for project in projects:
-        Flag=False
-        prereqs = str(project.PreRequisite).split('\r\n')
-        if(len(prereqs)>2):
-            Flag=True
-        prereqs= prereqs[0:2]
-        if(Flag==True):
-            prereqs.append("More Tags...")
-        project.PreRequisite = prereqs[0:3]
-
 
     context = {
         'title': 'Home',
         'allusers':User.objects.all(),
         'projects': projects,
-        'user_projects_id': user_projects_id,
-        'user_liked_projects_id':user_liked_projects_id,
-        'user_starred_projects_id' : user_starred_projects_id,
-        'user_requested_projects_id' : user_requested_projects_id,
+        'user_projects_id': projects_id[0],
+        'user_starred_projects_id' : projects_id[1],
+        'user_requested_projects_id' : projects_id[2],
+        'user_liked_projects_id':projects_id[3],
         'num_projects_applied' : user_applied_projects.count(),
-        'num_projects_req':len(user_requested_projects_id),
+        'num_projects_req':len(projects_id[2]),
         'num_projects_floated': user_floated_projects.count(),
         'notifications': Notification.objects.filter(user = request.user).order_by('-time'),
         'myFilter':myFilter,
@@ -125,32 +74,14 @@ def project(request):
     project = Project.objects.get(id=project_id)
     apply_requests = project.ApplyRequest.all()
 
-    all_project_list = Project.objects.all().order_by('-DatePosted')
-    user_projects_id = []
-    user_starred_projects_id = []
-    user_requested_projects_id = []
-
-    user_applied_projects = all_project_list.filter(AlreadyApplied =  request.user)
-    user_floated_projects = all_project_list.filter(FloatedBy =  request.user)
-    for tempproject in user_applied_projects:
-        user_projects_id.append(tempproject.id)
-    for tempproject in user_floated_projects:
-        user_projects_id.append(tempproject.id)
-
-    user_requested_projects = all_project_list.filter(ApplyRequest = request.user)
-    for tempproject in user_requested_projects:
-        user_requested_projects_id.append(tempproject.id)
-
-    for tempproject in request.user.profile.starred_projects.all():
-        user_starred_projects_id.append(tempproject.id)
-
+    projects_id = get_projects_id(request)
 
     context = {
         'title': 'Project',
         'project': project,
-        'user_projects_id': user_projects_id,
-        'user_starred_projects_id' : user_starred_projects_id,
-        'user_requested_projects_id' : user_requested_projects_id,
+        'user_projects_id': projects_id[0],
+        'user_starred_projects_id' : projects_id[1],
+        'user_requested_projects_id' : projects_id[2],
         'apply_requests' : apply_requests,
         'notifications': Notification.objects.filter(user = request.user).order_by('-time'),
     }
@@ -187,7 +118,6 @@ def projectUpdate(request):
 @user_is_project_author
 def projectDelete(request):
     project_id = request.GET.get('project_id')
-    print(project_id)
     project = Project.objects.get(id=project_id)
     if request.method == 'POST':
         project.delete()
@@ -209,70 +139,12 @@ def projectTask(request):
     task = request.GET.get('task')
     page_number = request.GET.get('page_number')
 
-    project = Project.objects.get(id=project_id)
-    current_user = request.user
-
-    if task == "Apply":
-        year= current_user.profile.year
-        branch=current_user.profile.branch
-        if(len(year)==0 or len(branch)==0):
-            messages.warning(request,"Please update your year and branch in profile section")
-            return redirect('/profile/edit')
-        else:
-            user_branch = f"{year} Year {branch}"
-            if(user_branch in project.OpenedFor):
-                project.ApplyRequest.add(current_user)
-                messages.success(request,"Successfully Requested!")
-                notification_message=f"{current_user} has requested for {project} Project"
-                project_url = f"/project/?project_id={project_id}"
-                notification=Notification(user=project.FloatedBy, project_requested = project, notification_from = request.user, title= "Apply Request", message=notification_message,url=project_url)
-                notification.save()
-
-                # Mailing Work
-                if project.MailNotification == "On":
-                    subject = 'Project Application'
-                    message = notification_message
-                    email_from = settings.EMAIL_HOST_USER
-                    recipient_list = [project.FloatedBy.email,]
-                    send_mail( subject, message, email_from, recipient_list )
-            else:
-                messages.warning(request,"You are not eligible to opt this project.")
-    if task == "Withdraw":
-        Notification.objects.filter(notification_from = request.user).filter(project_requested = project).delete()
-        project.ApplyRequest.remove(current_user)
-    if task == "Leave":
-        if project.FloatedBy != current_user:
-            project.AlreadyApplied.remove(current_user)
-            messages.success(request,f"{project} is dropped successfully.")
-        else:
-            messages.error(request, 'You cannot leave this project because it is floated by you.')
-    if task == "Star":
-        current_user.profile.starred_projects.add(project)
-    if task == "Unstar":
-        current_user.profile.starred_projects.remove(project)
-
-    if task == "Like":
-        current_user.profile.liked_projects.add(project)
-        project.Likes += 1
-        project.save()
-        return HttpResponse("Liked!")
-        # user_liked_projects = request.user.profile.liked_projects.all()
-        # user_liked_projects_id=[]
-        # for project in user_liked_projects:
-        #     user_liked_projects_id.append(project.id)
-
-        # context = {
-        #     'user_liked_projects_id':user_liked_projects_id
-        # }
-        # return JsonResponse(context)
-    if task == "Unlike":
-        current_user.profile.liked_projects.remove(project)
-        project.Likes -= 1
-        project.save()
+    do_simple_task(request, task)
 
     if page_number:
         url = f'/?page={page_number}'
     else:
+        project = Project.objects.get(id=project_id)
         url = f'/project/?project_id={project.id}'
     return redirect(url)
 
