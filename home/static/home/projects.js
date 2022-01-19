2495e05c16438705a292fd03f92d0e5eb47e5efb
lazyXghost <rjtbnslrealme@gmail.com>
2022-01-19 22:43:33 +0530

    Revert "Added django-select2 in fields in project register"
    This reverts commit ec43ac77b684479446dffb10d7d0f129b6553ae0.

--- home/static/home/projects.js
+++ home/static/home/projects.js
@@ -1,67 +1,67 @@
-// let mentor, tag, att, target, inputValue;
-// $(document).ready(function () {
-//     mentor = document.getElementById("id_Mentors");
-//     att = document.createAttribute("multiple");
-//     att.value = "multiple";
-//     mentor.setAttributeNode(att);
+let mentor, tag, att, target, inputValue;
+$(document).ready(function () {
+    mentor = document.getElementById("id_Mentors");
+    att = document.createAttribute("multiple");
+    att.value = "multiple";
+    mentor.setAttributeNode(att);
 
-//     $("#id_Mentors").select2({
-//         allowClear: 'true'
-//     });
+    $("#id_Mentors").select2({
+        allowClear: 'true'
+    });
 
-//     tag = document.getElementById("id_Tags");
-//     att = document.createAttribute("multiple");
-//     att.value = "multiple";
-//     tag.setAttributeNode(att);
+    tag = document.getElementById("id_Tags");
+    att = document.createAttribute("multiple");
+    att.value = "multiple";
+    tag.setAttributeNode(att);
 
-//     $("#id_Tags").select2({
-//         "allowClear": 'true',
-//         "language": {
-//             "noResults": function () {
-//                 return "No Results Found <br> <a class='newTag'>Create New Tag</a>";
-//             }
-//         },
-//         escapeMarkup: function (markup) {
-//             return markup;
-//         }
-//     });
+    $("#id_Tags").select2({
+        "allowClear": 'true',
+        "language": {
+            "noResults": function () {
+                return "No Results Found <br> <a class='newTag'>Create New Tag</a>";
+            }
+        },
+        escapeMarkup: function (markup) {
+            return markup;
+        }
+    });
 
-//     let targetSelector = "#div_id_Tags > div > span > span.selection > span > ul > li.select2-search.select2-search--inline > input";
-//     target = document.querySelector(targetSelector);
-//     target.addEventListener("keyup", function () {
-//         inputValue = target.value;
-//     });
-// });
+    let targetSelector = "#div_id_Tags > div > span > span.selection > span > ul > li.select2-search.select2-search--inline > input";
+    target = document.querySelector(targetSelector);
+    target.addEventListener("keyup", function () {
+        inputValue = target.value;
+    });
+});
 
-// $(document).on('click', '.newTag', function () {
-//     var newTagTitle = inputValue;
-//     $.ajaxSetup({
-//         cache: false
-//     });
-//     $.ajax({
-//         type: "GET",
-//         url: `/tag/new/?newTagTitle=${newTagTitle}`,
-//         success: function (response) {
-//             $("#div_id_Tags > div").load(location.href + " #id_Tags", function (responseTxt, statusTxt, xhr) {
-//                 if (statusTxt == "success") {
-//                     $("#id_Tags").select2({
-//                         "allowClear": 'true',
-//                         "language": {
-//                             "noResults": function () {
-//                                 return "No Results Found <br> <a class='newTag'>Create New Tag</a>";
-//                             }
-//                         },
-//                         escapeMarkup: function (markup) {
-//                             return markup;
-//                         }
-//                     });
-//                 }
-//                 if (statusTxt == "error")
-//                     alert("Error: " + xhr.status + ": " + xhr.statusText);
-//                 });
-//             },
-//             error: function (rs, e) {
-//             console.log(rs.responseText);
-//         },
-//     });
-// });
+$(document).on('click', '.newTag', function () {
+    var newTagTitle = inputValue;
+    $.ajaxSetup({
+        cache: false
+    });
+    $.ajax({
+        type: "GET",
+        url: `/tag/new/?newTagTitle=${newTagTitle}`,
+        success: function (response) {
+            $("#div_id_Tags > div").load(location.href + " #id_Tags", function (responseTxt, statusTxt, xhr) {
+                if (statusTxt == "success") {
+                    $("#id_Tags").select2({
+                        "allowClear": 'true',
+                        "language": {
+                            "noResults": function () {
+                                return "No Results Found <br> <a class='newTag'>Create New Tag</a>";
+                            }
+                        },
+                        escapeMarkup: function (markup) {
+                            return markup;
+                        }
+                    });
+                }
+                if (statusTxt == "error")
+                    alert("Error: " + xhr.status + ": " + xhr.statusText);
+                });
+            },
+            error: function (rs, e) {
+            console.log(rs.responseText);
+        },
+    });
+});
--- project_portal/urls.py
+++ project_portal/urls.py
@@ -24,7 +24,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("select2/", include("django_select2.urls")),
     path('accounts/', include('allauth.urls')),
     path('profile/', include('users.urls')),
     path('signup/', users_views.signup, name='signup'),
