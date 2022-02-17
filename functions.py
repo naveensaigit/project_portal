2b2272d52c8f2545bcfd6fadc2bc591f8a0b2055
lazyXghost <rjtbnslrealme@gmail.com>
2022-02-17 20:13:04 +0530

    Completed apply reject and apply request function

--- functions.py
+++ functions.py
@@ -171,31 +171,32 @@ def apply_on_project(request, project):
 def withdraw_from_project(request, project):
     current_user = request.user
     delete_notification(request, project)
-    ApplyRequest.objects.all().get(User = current_user).delete()
+    ApplyRequest.objects.all().get(User = current_user).filter(Project = project).delete()
 
 def leave_project(request, project):
     current_user = request.user
     if project.FloatedBy != current_user:
         project.AlreadyApplied.remove(current_user)
         ApplyRequest.objects.all().get(User = current_user).delete()
+        delete_notification(request, project)
         messages.success(request,f"{project} is dropped successfully.")
     else:
         messages.error(request, 'You cannot leave this project because it is floated by you.')
 
-def all_apply_requests_task(project, task):
+def all_apply_requests_task(request, project, task):
     apply_requests = ApplyRequest.objects.all().filter(Project=project)
     for apply_request in apply_requests:
         if(task=="AcceptAll"):
-            apply_request_task(apply_request, "Accept")
+            apply_request_task(request, apply_request, "Accept")
         else:
-            apply_request_task(apply_request, "Reject")
+            apply_request_task(request, apply_request, "Reject")
 
-def apply_request_task(apply_request,task):
-    print(apply_request)
+def apply_request_task(request, apply_request,task):
     newStatus = task + "ed"
     apply_request.Status = newStatus
     if newStatus == "Accepted":
         apply_request.Project.AlreadyApplied.add(apply_request.User)
+    delete_notification(request, apply_request.Project)
     apply_request.save()
 
 def do_task(request):
@@ -220,18 +221,17 @@ def do_task(request):
         project.Likes -= 1
         project.save()
     elif task == "Apply":
-        # perform notification, applyrequest, project already applied changes
         apply_on_project(request, project)
     elif task == "Withdraw":
         withdraw_from_project(request,project)
     elif task == "Accept" or task == "Reject":
         request_user_name = request.GET.get('request_user')
         request_user = User.objects.filter(username = request_user_name).first()
+        apply_request = ApplyRequest.objects.all().filter(User = request_user).filter(Project = project)
 
-        if task == "Accept":
-            project.AlreadyApplied.add(request_user)
+        apply_request_task(request, apply_request, task)
     elif task == "AcceptAll" or task == "RejectAll":
-        all_apply_requests_task(project, task)
+        all_apply_requests_task(request, project, task)
 
 def get_projects_view_details(request):
     view = request.GET.get('view')
