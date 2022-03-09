8de45f9c4c5c4fb322a4b095c03a69a2024ae08f
lazyXghost <rjtbnslrealme@gmail.com>
2022-03-09 11:37:35 +0530

    Made some changes for making profile page responsive

--- users/static/users/profile.js
+++ users/static/users/profile.js
@@ -5,17 +5,18 @@ var visibleDivId = 'projects0';
 $(document).ready(function () {
     var elem = document.getElementById("projects0");
     elem.classList.remove("invisible");
+    makeChanges();
 });
 
 function toggleVisibility(divId) {
     if (visibleDivId != divId) {
         var toHideElem = document.getElementById(visibleDivId);
-        var toHideElemButton = document.getElementById(visibleDivId+"button");
+        var toHideElemButton = document.getElementById(visibleDivId + "button");
         toHideElemButton.classList.remove('buttonSelected')
         toHideElem.classList.add('invisible');
 
         var toShowElem = document.getElementById(divId);
-        var toShowElemButton = document.getElementById(divId+"button");
+        var toShowElemButton = document.getElementById(divId + "button");
         toShowElemButton.classList.add('buttonSelected')
         toShowElem.classList.remove('invisible');
 
@@ -23,3 +24,30 @@ function toggleVisibility(divId) {
     }
 }
 
+function changeSize(x, size2, size1) {
+    // size1 750
+    // size2 1500
+
+    // m = (size2 - size1)/750
+    // c = size2 - m*1500
+
+    // size = m*w + c 
+    var w, m, c;
+    w = window.innerWidth;
+    m = (size2 - size1) / 650;
+    c = size2 - m * 1500;
+    x = String(m * w + c) + "px";
+}
+
+function makeChanges() {
+    var elem = document.getElementsByClassName('projectButtonTitle');
+    for (var i = 0; i < elem.length; i++) {
+        changeSize(elem[i].style.fontSize, 15, 10);
+    }
+
+    elem = document.getElementsByClassName('projectsNumbers');
+    for (var i = 0; i < elem.length; i++) {
+        changeSize(elem[i].style.fontSize, 20, 15);
+    }
+}
+window.onresize = makeChanges;
