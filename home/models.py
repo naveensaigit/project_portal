a50feafedf00ba5d4811f680c36ac7c99bb9f88c
lazyXghost <rjtbnslrealme@gmail.com>
2022-03-10 11:56:03 +0530

    Corrected duration field options

--- home/models.py
+++ home/models.py
@@ -14,12 +14,12 @@ DIFFICULTY_CHOICES = (
 )
 
 DURATION_CHOICES = (
-    ('1', '< 1 month'),
-    ('1-2', '1-2 months'),
-    ('2-4', '2-4 months'),
-    ('4-6', '4-6 months'),
-    ('6-12', '6-12 months'),
-    ('12', '> 12 months'),
+    ('<1 month', '< 1 month'),
+    ('1-2 months', '1-2 months'),
+    ('2-4 months', '2-4 months'),
+    ('4-6 months', '4-6 months'),
+    ('6-12 months', '6-12 months'),
+    ('>12 months', '> 12 months'),
     
 )
 
