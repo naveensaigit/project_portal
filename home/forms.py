1cfb6f25407e1ead92dda62d6c7ea0993d919ca7
lazyXghost <rjtbnslrealme@gmail.com>
2023-03-30 12:12:00 +0530

    Updated project updation form

--- home/forms.py
+++ home/forms.py
@@ -4,55 +4,63 @@ from django.forms.widgets import FileInput
 
 OPENED_FOR_CHOICES = (
     ('All','All'),
-    ('CSE',(
-        ('CSE 1st Year', 'CSE 1st Year'),
-        ('CSE 2nd Year', 'CSE 2nd Year'),
-        ('CSE 3rd Year', 'CSE 3rd Year'),
-        ('CSE 4th Year', 'CSE 4th Year'),
-        )
-    ),
-    ('DSE',(
-        ('DSE 1st Year', 'DSE 1st Year'),
-        ('DSE 2nd Year', 'DSE 2nd Year'),
-        ('DSE 3rd Year', 'DSE 3rd Year'),
-        ('DSE 4th Year', 'DSE 4th Year'),
-        )
-    ),
-    ('EE',(
-        ('EE 1st Year', 'EE 1st Year'),
-        ('EE 2nd Year', 'EE 2nd Year'),
-        ('EE 3rd Year', 'EE 3rd Year'),
-        ('EE 4th Year', 'EE 4th Year'),
-        )
-    ),
-    ('ME',(
-        ('ME 1st Year', 'ME 1st Year'),
-        ('ME 2nd Year', 'ME 2nd Year'),
-        ('ME 3rd Year', 'ME 3rd Year'),
-        ('ME 4th Year', 'ME 4th Year'),
-        )
-    ),
-    ('EP',(
-        ('EP 1st Year', 'EP 1st Year'),
-        ('EP 2nd Year', 'EP 2nd Year'),
-        ('EP 3rd Year', 'EP 3rd Year'),
-        ('EP 4th Year', 'EP 4th Year'),
-        )
-    ),
-    ('CE',(
-        ('CE 1st Year', 'CE 1st Year'),
-        ('CE 2nd Year', 'CE 2nd Year'),
-        ('CE 3rd Year', 'CE 3rd Year'),
-        ('CE 4th Year', 'CE 4th Year'),
-        )
-    ),
-    ('BioE',(
-        ('BioE 1st Year', 'BioE 1st Year'),
-        ('BioE 2nd Year', 'BioE 2nd Year'),
-        ('BioE 3rd Year', 'BioE 3rd Year'),
-        ('BioE 4th Year', 'BioE 4th Year'),
-        )
-    ),
+    ('Faculty','Faculty'),
+    ('Staff','Staff'),
+    ('Students',(
+        ('B.Tech',(
+            ('CSE',(
+                ('CSE 1st Year', 'CSE 1st Year'),
+                ('CSE 2nd Year', 'CSE 2nd Year'),
+                ('CSE 3rd Year', 'CSE 3rd Year'),
+                ('CSE 4th Year', 'CSE 4th Year'),
+                )
+            ),
+            ('DSE',(
+                ('DSE 1st Year', 'DSE 1st Year'),
+                ('DSE 2nd Year', 'DSE 2nd Year'),
+                ('DSE 3rd Year', 'DSE 3rd Year'),
+                ('DSE 4th Year', 'DSE 4th Year'),
+                )
+            ),
+            ('EE',(
+                ('EE 1st Year', 'EE 1st Year'),
+                ('EE 2nd Year', 'EE 2nd Year'),
+                ('EE 3rd Year', 'EE 3rd Year'),
+                ('EE 4th Year', 'EE 4th Year'),
+                )
+            ),
+            ('ME',(
+                ('ME 1st Year', 'ME 1st Year'),
+                ('ME 2nd Year', 'ME 2nd Year'),
+                ('ME 3rd Year', 'ME 3rd Year'),
+                ('ME 4th Year', 'ME 4th Year'),
+                )
+            ),
+            ('EP',(
+                ('EP 1st Year', 'EP 1st Year'),
+                ('EP 2nd Year', 'EP 2nd Year'),
+                ('EP 3rd Year', 'EP 3rd Year'),
+                ('EP 4th Year', 'EP 4th Year'),
+                )
+            ),
+            ('CE',(
+                ('CE 1st Year', 'CE 1st Year'),
+                ('CE 2nd Year', 'CE 2nd Year'),
+                ('CE 3rd Year', 'CE 3rd Year'),
+                ('CE 4th Year', 'CE 4th Year'),
+                )
+            ),
+            ('BioE',(
+                ('BioE 1st Year', 'BioE 1st Year'),
+                ('BioE 2nd Year', 'BioE 2nd Year'),
+                ('BioE 3rd Year', 'BioE 3rd Year'),
+                ('BioE 4th Year', 'BioE 4th Year'),
+                )
+            )
+        )),
+        ('PHD', 'PHD'),
+        ('M.Tech', 'M.Tech')
+    )), 
 )
 
 class ProjectRegisterForm(forms.ModelForm):
